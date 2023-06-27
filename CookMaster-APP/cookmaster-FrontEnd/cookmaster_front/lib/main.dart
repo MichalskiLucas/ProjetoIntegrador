203af54daf3906b5c1d70a7994e78b7d24d8aafb
MichalskiLucas <95660732+MichalskiLucas@users.noreply.github.com>
2023-06-27 20:09:51 -0300

    Feat: Login button created

--- CookMaster-APP/cookmaster-FrontEnd/cookmaster_front/lib/main.dart
+++ CookMaster-APP/cookmaster-FrontEnd/cookmaster_front/lib/main.dart
@@ -39,7 +39,79 @@ Widget _listCookMasterLogin() {
             style: TextStyle(fontSize: 25, fontFamily: 'JacquesFrancois'),
           )
         ],
-      )
+      ),
+      Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(height: 200),
+          SizedBox(
+            width: 235,
+            height: 58,
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                    (Set<MaterialState> states) {
+                  if (states.contains(MaterialState.pressed)) {
+                    return Colors.orange.shade900;
+                  }
+                  return Colors.orange.shade900;
+                }),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50.0),
+                        side: const BorderSide(color: Colors.orange))),
+              ),
+              onPressed: () {
+                print('Cliquei no elevatedButton');
+              },
+              child: const Text(
+                'Entrar sem Login',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'JacquesFrancois',
+                    fontSize: 15),
+              ),
+            ),
+          )
+        ],
+      ),
+      Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(height: 20),
+          SizedBox(
+            width: 235,
+            height: 58,
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                    (Set<MaterialState> states) {
+                  if (states.contains(MaterialState.pressed)) {
+                    return Colors.grey.shade600;
+                  }
+                  return Colors.grey.shade600;
+                }),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50.0),
+                        side: const BorderSide(color: Colors.grey))),
+              ),
+              onPressed: () {
+                print('Cliquei no elevatedButton');
+              },
+              child: const Text(
+                'Entrar com o Google',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'JacquesFrancois',
+                    fontSize: 15),
+              ),
+            ),
+          )
+        ],
+      ),
     ],
   );
 }
