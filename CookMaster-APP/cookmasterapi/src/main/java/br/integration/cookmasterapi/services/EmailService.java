f460eaa6c0c9a9a89bbb599372f7be6993bd461d
Kedssy <kedssy533779@gmail.com>
2023-11-20 22:09:28 -0300

    refactory: criando classe de email
    Criada classe de envio de email

--- CookMaster-APP/cookmasterapi/src/main/java/br/integration/cookmasterapi/services/EmailService.java
+++ CookMaster-APP/cookmasterapi/src/main/java/br/integration/cookmasterapi/services/EmailService.java
@@ -1,5 +1,6 @@
 package br.integration.cookmasterapi.services;
 
+import br.integration.cookmasterapi.model.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.util.List;
 
 @Service
 public class EmailService {
@@ -14,7 +16,10 @@ public class EmailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
-    public void sendMail(String dest, String assunto, String corpo) throws Exception {
+    @Autowired
+    UsuarioService usuarioService;
+
+    private void sendMail(String dest, String assunto, String corpo) throws Exception {
         MimeMessage mensagem = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(mensagem, true);
 
@@ -23,10 +28,31 @@ public class EmailService {
         helper.setText(corpo, false);
         try {
             javaMailSender.send(mensagem);
-        }catch (Exception ex){
+        } catch (Exception ex) {
             throw new Exception(ex.getMessage());
         }
 
+    }
+
+
+    public void sendEmailRecipe(long idReceita) throws Exception {
+        String corpo = "Receita com id: " + idReceita + " encontra-se pendente de aprovação.";
+        String assunto = "Receita pendente de aprovação";
+        List<Usuario> usuarios = usuarioService.findAdmin();
+
+        for (int i = 0; i < usuarios.size(); i++) {
+            sendMail(usuarios.get(i).getEmail(), corpo, assunto);
+        }
+    }
+
+    public void sendEmailIngredient(long idIngrediente) throws Exception {
+        String corpo = "Ingrediente com id: " + idIngrediente + " encontra-se pendente de aprovação.";
+        String assunto = "Ingredente pendente de aprovação";
+        List<Usuario> usuarios = usuarioService.findAdmin();
+
+        for (int i = 0; i < usuarios.size(); i++) {
+            sendMail(usuarios.get(i).getEmail(), corpo, assunto);
+        }
     }
 
 }
